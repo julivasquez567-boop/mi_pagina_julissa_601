7a670ef41749747c24068220bb1cab654bd2b01f
julivasquez567-boop <julivasquez567@gmail.com>
2026-04-14 11:25:31 -0600

    correccion

---
+++ script.js
@@ -0,0 +1,11 @@
+function mostrarMensaje() {
+    let mensaje = document.getElementById("mensaje");
+
+    mensaje.innerHTML = "🐾 Cuida y protege a los animales";
+    mensaje.style.display = "block";
+
+    setTimeout(() => {
+        mensaje.style.opacity = "1";
+        mensaje.style.transform = "translateY(0)";
+    }, 50);
+}
