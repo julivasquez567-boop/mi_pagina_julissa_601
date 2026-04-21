f0b06251377e2b097fc0c5bed942928e7b923597
julivasquez567-boop <julivasquez567@gmail.com>
2026-04-21 14:26:12 -0600

    mensaje oculto

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
 function mostrarMensaje() {
     let mensaje = document.getElementById("mensaje");
+    let boton = event.target;
 
-    mensaje.innerHTML = "🐾 Cuida y protege a los animales 🐾";
-    mensaje.style.display = "block";
+    if (mensaje.style.opacity === "1") {
+        // OCULTAR
+        mensaje.style.opacity = "0";
+        mensaje.style.transform = "translateY(30px)";
+        boton.innerText = "Mostrar mensaje";
+    } else {
+        // MOSTRAR
+        mensaje.innerHTML = "🐾 Cuida y protege a los animales 🐾";
+        mensaje.style.display = "block";
 
-    setTimeout(() => {
-        mensaje.style.opacity = "1";
-        mensaje.style.transform = "translateY(0)";
-        mensaje.style.textAlign = "center";
-    }, 50);
+        setTimeout(() => {
+            mensaje.style.opacity = "1";
+            mensaje.style.transform = "translateY(0)";
+            mensaje.style.textAlign = "center";
+        }, 50);
+
+        boton.innerText = "Ocultar mensaje";
+    }
 }
 
 function mostrarMensaje2() {
     let mensaje = document.getElementById("mensaje2");
+    let boton = event.target;
 
-    mensaje.innerHTML = "🎧 Mi pasatiempo favorito es escuchar música 🎧";
-    mensaje.style.display = "block";
+    if (mensaje.style.opacity === "1") {
+        // OCULTAR
+        mensaje.style.opacity = "0";
+        mensaje.style.transform = "translateY(30px)";
+        boton.innerText = "Algo sobre mí";
+    } else {
+        // MOSTRAR
+        mensaje.innerHTML = "🎧 Mi pasatiempo favorito es escuchar música 🎧";
+        mensaje.style.display = "block";
 
-    setTimeout(() => {
-        mensaje.style.opacity = "1";
-        mensaje.style.transform = "translateY(0)";
-        mensaje.style.textAlign = "center";
-    }, 50);
+        setTimeout(() => {
+            mensaje.style.opacity = "1";
+            mensaje.style.transform = "translateY(0)";
+            mensaje.style.textAlign = "center";
+        }, 50);
+
+        boton.innerText = "Ocultar mensaje";
+    }
 }
